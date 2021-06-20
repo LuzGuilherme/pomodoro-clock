dd0ac5693921bdcfeb38b7951acaadf1e584f073
Guilherme Duarte da Luz <gduartedaluz@gmail.com>
2021-06-20 15:22:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function App() {
-  const [displayTime, setDisplayTime] = React.useState(70);
-  const [breakTime, setBreakTime] = React.useState(65);
-  const [sessionTime, setSessionTime] = React.useState(70);
+  const [displayTime, setDisplayTime] = React.useState(25*50);
+  const [breakTime, setBreakTime] = React.useState(5*60);
+  const [sessionTime, setSessionTime] = React.useState(25*60);
   const [timerOn, setTimerOn] = React.useState(false);
   const [onBreak, setOnBreak] = React.useState(false);
   const [breakAudio, setBreakAudio] = React.useState(
